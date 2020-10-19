df5a507004df27627f0dd53bd9a30e27c43ba3fa
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-10-19 23:24:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ module.exports = async(req, res) => {
             res.json({ result: 'false' });
         } else {
             var unix = Number(response.body) + Number('19800');
-            var timem = moment.unix(unix).locale('en-in');
+            var timem = moment.unix(unix);
             res.json({
                 timezone: 'Asia/Kolkata',
                 unixtime: unix,
